24d99e1d43a11d8d2066621e5ac190c6d1a6abab
Caio IESB Hudson UnB <caiohudson.red4@hotmail.com>
2025-06-05 18:13:38 -0300

    Global Dictionary for the SBC Directives

---
+++ Scripts/global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# This is where the SBC Directives are stored
+# It will be a GLOBAL Dictionary, so that any script can access it to use
+# Will be used more in the custom BBCodeLink for the PageResource
+
+var SBC_DIRECTIVES = {
+	"1.1" : "1.1 - Directive ",
+	"1.2" : "1.2 - ",
+	"1.3" : "1.3 - ",
+	"2.1" : "2.1 - ",
+	"2.2" : "2.2 - ",
+	"2.3" : "2.3 - ",
+	"3.1" : "3.1 - ",
+	"3.2" : "3.2 - ",
+	"3.3" : "3.3 - ",
+}
--- Scripts/page_resource.gd
+++ Scripts/page_resource.gd
@@ -1,6 +1,12 @@
 @tool
 class_name PageResource extends Resource
 
+# In order to create the mistakes, we're going to Highlight them by
+# using a custom BB Code Marker named [wrong "_sbcDirective"][/wrong] that highlights
+# the sentence that is wrong based on a directive from the SBC Document
+
+# Example: [wrong "3"]"We got a chamber here"[/wrong] 
+
 @export_group("Page Properties")
 @export_multiline var title : String = "Insert Title of Page"
 @export_multiline var software_desc : String = "Software Description"
@@ -8,5 +14,6 @@ class_name PageResource extends Resource
 @export_multiline var management_desc : String = "How the software will hold up"
 @export_multiline var workplace_desc : String = "Workplace ambient"
 
-@export_group("Key Sentences")
-@export var sentences : Array[PageSentence]
+
+@export_group("")
+@export var isWrong : bool
