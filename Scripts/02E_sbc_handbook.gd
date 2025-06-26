f27cfb7999726e0e1abf64d99288463da47a9509
João Victor Prata <202028857@aluno.unb.br>
2025-06-26 11:40:39 -0300

    turn page sounds for sbc handbook and updated handbook pages

--- Scripts/02E_sbc_handbook.gd
+++ Scripts/02E_sbc_handbook.gd
@@ -2,12 +2,19 @@
 class_name SBCHandbook
 extends Node2D
 
-const PAGE_COUNT := 4
+const PAGE_COUNT := 8
 const PAGE_PATH_TEMPLATE := "res://Assets/SBC Handbook Pages/SBC_Handbook_Page_%d.png"
 
 @onready var page_texture: Sprite2D = $Page_Texture
 @onready var next_button: Button = $NextPageButton
 @onready var prev_button: Button = $PreviousPageButton
+@onready var background_page = $BackgroundPage
+
+@onready var turn_page_sound_1 = $TurnPageSound1
+@onready var turn_page_sound_2 = $TurnPageSound2
+@onready var turn_page_sound_3 = $TurnPageSound3
+
+@onready var sounds_turn_page = [turn_page_sound_1, turn_page_sound_2, turn_page_sound_3]
 
 var current_page := 1
 
@@ -18,11 +25,15 @@ func _on_next_page_button_pressed() -> void:
 	if current_page < PAGE_COUNT:
 		current_page += 1
 		update_page()
+		var random_sound_turn_page = sounds_turn_page[randi() % sounds_turn_page.size()]
+		random_sound_turn_page.play()
 
 func _on_previous_page_button_pressed() -> void:
 	if current_page > 1:
 		current_page -= 1
 		update_page()
+		var random_sound_turn_page = sounds_turn_page[randi() % sounds_turn_page.size()]
+		random_sound_turn_page.play()
 
 func update_page() -> void:
 	var page_path := PAGE_PATH_TEMPLATE % current_page
@@ -40,3 +51,5 @@ func update_page() -> void:
 	next_button.visible = current_page < PAGE_COUNT
 	next_button.disabled = current_page == PAGE_COUNT
 	
+	# Hide background page if this is the last page
+	#background_page.visible = current_page != PAGE_COUNT
